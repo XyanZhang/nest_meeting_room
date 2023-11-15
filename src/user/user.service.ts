78ea97a55fbbd5266487041f4fb6c2b1124a5625
XyanZhang <1743067175@qq.com>
2023-11-15 22:21:54 +0800

    feat: userRepository

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
+  @InjectRepository(User)
+  private userRepository: Repository<User>;
+
   create(createUserDto: CreateUserDto) {
     // 利用typeorm 写入数据库
-    
+    console.log(this.userRepository)
     return 'This action adds a new user';
   }
 
