125b81973188620a660e0e56667828108607809a
XyanZhang <1743067175@qq.com>
2023-11-23 14:55:15 +0800

    feat: 引入swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { FormatResponseInterceptor } from './format-response.interceptor';
 import { InvokeRecordInterceptor } from './invoke-record.interceptor';
 import { UnloginFilter } from './unlogin.filter';
@@ -15,6 +16,14 @@ async function bootstrap() {
   app.useGlobalInterceptors(new InvokeRecordInterceptor())
   app.useGlobalFilters(new UnloginFilter())
   app.useGlobalFilters(new CustomExceptionFilter())
+
+  const config = new DocumentBuilder()
+    .setTitle('会议预订系统')
+    .setDescription('api 接口文档')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
 
   let configService = app.get(ConfigService)
   await app.listen(configService.get('nest_server_port'));
