bf660b367001418db29accdc158c1616714b0e81
XyanZhang <1743067175@qq.com>
2023-11-24 15:00:38 +0800

    feat: 会议室增删改查

--- src/meeting-room/meeting-room.service.ts
+++ src/meeting-room/meeting-room.service.ts
@@ -1,15 +1,96 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { CreateMeetingRoomDto } from './dto/create-meeting-room.dto';
 import { UpdateMeetingRoomDto } from './dto/update-meeting-room.dto';
 import { MeetingRoom } from './entities/meeting-room.entity';
 
 @Injectable()
 export class MeetingRoomService {
-  
   @InjectRepository(MeetingRoom)
   private repository: Repository<MeetingRoom>;
+
+  async find(pageNo: number, pageSize: number, name: string, capacity: number, equipment: string) {
+    if (pageNo < 1) {
+      throw new BadRequestException('页码最小为 1');
+    }
+    const skipCount = (pageNo - 1) * pageSize;
+
+    const condition: Record<string, any> = {};
+    
+    if(name) {
+      condition.name = Like(`%${name}%`);   
+    }
+    if(equipment) {
+        condition.equipment = Like(`%${equipment}%`); 
+    }
+    if(capacity) {
+        condition.capacity = capacity;
+    }
+
+    const [meetingRooms, totalCount] = await this.repository.findAndCount({
+      skip: skipCount,
+      take: pageSize,
+      where: condition
+    });
+
+    return {
+      meetingRooms,
+      totalCount,
+    };
+  }
+
+  
+  async create(meetingRoomDto: CreateMeetingRoomDto) {
+    const room = await this.repository.findOneBy({
+      name: meetingRoomDto.name
+    });
+
+    if(room) {
+      throw new BadRequestException('会议室名字已存在');
+    }
+
+    return await this.repository.save(meetingRoomDto);
+  }
+
+  async findById(id: number) {
+    return this.repository.findOneBy({
+      id
+    }); 
+  }
+
+  async update(meetingRoomDto: UpdateMeetingRoomDto) {
+    const meetingRoom = await this.repository.findOneBy({
+      id: meetingRoomDto.id
+    })
+
+    if(!meetingRoom) {
+      throw new BadRequestException('会议室不存在');
+    }
+
+    meetingRoom.capacity = meetingRoomDto.capacity;
+    meetingRoom.location = meetingRoomDto.location;
+    meetingRoom.name = meetingRoomDto.name;
+
+    if(meetingRoomDto.description) {
+      meetingRoom.description = meetingRoomDto.description;
+    }
+    if(meetingRoomDto.equipment) {
+      meetingRoom.equipment = meetingRoomDto.equipment;
+    }
+
+    await this.repository.update({
+      id: meetingRoom.id
+    } , meetingRoom);
+    return 'success';
+  }
+
+  async delete(id: number) {
+    await this.repository.delete({
+      id
+    });
+    return 'success';
+  }
 
   initData() {
     const room1 = new MeetingRoom();
@@ -33,25 +114,5 @@ export class MeetingRoomService {
     // save 确定是 insert 的时候 用 insert 比用 save 更好，能够批量插入数据
     // 同理，确定是 update 的时候，也不要用 save，因为它会先 select 一次，再确定是 udpate 还是 insert
     this.repository.insert([room1, room2, room3]);
-  }
-
-  create(createMeetingRoomDto: CreateMeetingRoomDto) {
-    return 'This action adds a new meetingRoom';
-  }
-
-  findAll() {
-    return `This action returns all meetingRoom`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} meetingRoom`;
-  }
-
-  update(id: number, updateMeetingRoomDto: UpdateMeetingRoomDto) {
-    return `This action updates a #${id} meetingRoom`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} meetingRoom`;
   }
 }
